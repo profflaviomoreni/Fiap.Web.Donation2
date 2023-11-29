fee714f50f81d58f0cc8efa73e7a617990cceb9f
Flavio Moreni <PF0954@fiap.com.br>
2023-11-28 21:52:26 -0300

    Add project files.

---
+++ Fiap.Web.Donation2/Controllers/ContatoController.cs
@@ -0,0 +1,33 @@
+﻿using Fiap.Web.Donation2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fiap.Web.Donation2.Controllers
+{
+    public class ContatoController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Help()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Gravar(ContatoModel contatoModel)
+        {
+            // INSERT INTO --- contatoModel
+
+            return View("Sucesso");
+        }
+
+        
+
+
+    }
+}
---
+++ Fiap.Web.Donation2/Models/ContatoModel.cs
@@ -0,0 +1,17 @@
+﻿namespace Fiap.Web.Donation2.Models
+{
+    public class ContatoModel
+    {
+        public int ContatoId { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Sobrenome { get; set; }
+
+        public string Email { get; set; }
+
+        public string Telefone { get; set; }
+
+        public string Mensagem { get; set; }
+    }
+}
